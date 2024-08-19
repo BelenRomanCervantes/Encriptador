7afabb38b5e4f413d0939f8d4dababd40f1bc473
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2024-08-19 15:28:43 -0600

    Enhanced functionality

--- encriptador.js
+++ encriptador.js
@@ -1,30 +1,82 @@
-function encriptar() {
-    const inputText = document.getElementById('input-text').value;
-    const encryptedText = inputText
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
-    
-    document.getElementById('output-text').value = encryptedText;
+// Declarar constantes
+const inputText = document.getElementById("input-text");
+const encryptButton = document.getElementById("encrypt-button");
+const decryptButton = document.getElementById("decrypt-button");
+const copyButton = document.getElementById("copy-button");
+const finalMessage = document.getElementById("output-text");
+const image = document.getElementById("image");
+const info = document.getElementById("info");
+const presentation = document.getElementById("presentacion");
+
+// Llaves de encriptacion
+let keys = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+]
+
+// Funcion para mostrar el texto encriptado/desencriptado
+const result = (newValue) => {
+    finalMessage.innerHTML = newValue;
+
+    image.style.display = "none";
+    inputText.value = "";
+    info.style.display = "none";
+    copyButton.style.display = "block";
+    presentation.classList.add("ajustar");
+    finalMessage.classList.add("ajustar");
 }
 
-function desencriptar() {
-    const inputText = document.getElementById('input-text').value;
-    const decryptedText = inputText
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
-    
-    document.getElementById('output-text').value = decryptedText;
+// Funcion para encriptar el texto
+encryptButton.addEventListener("click", () => {
+    const text = inputText.value.toLowerCase()
+    if (text != "") {
+    function encrypt(newText) {
+        for (let i = 0; i < keys.length; i++){
+            if(newText.includes(keys[i][0])){
+                newText = newText.replaceAll(keys[i][0], keys[i][1]);
+            };
+        };
+        return newText;
+    }
+    result(encrypt(text));
+} else {
+    alert("Ingrese un texto para encriptarlo")
 }
+})
 
-function copiar() {
-    const outputText = document.getElementById('output-text');
-    outputText.select();
-    document.execCommand('copy');
-    alert('Texto copiado');
+// Funcion para desencriptar el texto
+decryptButton.addEventListener("click", () => {
+    const text = inputText.value.toLowerCase()
+    if (text != "") {
+    function decrypt(newText) {
+        for (let i = 0; i < keys.length; i++){
+            if(newText.includes(keys[i][1])){
+                newText = newText.replaceAll(keys[i][1], keys[i][0]);
+            };
+        };
+        return newText;
+    }
+    result(decrypt(text));
+} else {
+    alert("Ingrese un texto para desencriptarlo")
 }
+})
+
+copyButton.addEventListener("click", () => {
+    let text = finalMessage;
+    text.select();
+    document.execCommand("copy")
+    alert("Texto copiado");
+
+    finalMessage.innerHTML = "";
+    image.style.display = "block";
+    info.style.display = "block";
+    copyButton.style.display = "none";
+    presentation.classList.remove("ajustar");
+    finalMessage.classList.remove("ajustar");
+    inputText.focus();
+})
+
